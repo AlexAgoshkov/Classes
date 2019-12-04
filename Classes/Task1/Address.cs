ed47ad6c3a51030753882742cd6c2bf74a7e592a
Alexey <alex.agoshkov6@gmail.com>
2019-12-04 10:40:10 +0200

    Task1
    Task1

---
+++ Classes/Task1/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Address
+    {
+
+
+
+
+
+        private int index;
+
+        private string country;
+
+        private string city;
+
+        private string street;
+
+        private string house;
+
+        private string apartment;
+    }
+}
